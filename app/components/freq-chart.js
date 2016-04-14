ce1de3c5c44a30ccc31cfec502d53294d277da34
aliciakw <alicia.willett@gmail.com>
2016-04-14 12:21:56 -0400

    Render frequency data and scales

--- app/components/freq-chart.js
+++ app/components/freq-chart.js
@@ -1,39 +1,39 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  y_scale: [' ', ' ', ' ', ' ', ' ', 0],
-  x_scale: [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '],
+  data_scale: [],
+  data_frequency: [],
   y_intervals: 5,
   x_intervals: 5,
-  data_frequency: [],
-  sum: function () {
-    return this.values.reduce(function (a,b) {
-      return a + b;
-    });
+  y_scale_max: 0,
+  chart_height: 170,
+  chart_width: 200,
+  chart_body: function () {
+    return Ember.String.htmlSafe('height:'+ (this.chart_height + 10) +'px; width:' + (this.chart_width + 30) + 'px;')
   }.property(),
-  frequency: function () {
-    var x_intervals = this.x_intervals,
+  bar_width: function () {
+    return Ember.String.htmlSafe('width:'+ ((this.chart_width) / this.x_intervals) +'px;');
+  }.property(),
+  y_scale_dims: function () {
+    return Ember.String.htmlSafe('height:' + ((this.chart_height) / this.y_intervals) + 'px;');
+  }.property(),
+  frequency_scale: function () {
+    var x_range = calc_range(this.values, this.x_intervals),
+        intervals = this.x_intervals,
         y_intervals = this.y_intervals;
-    var x_range = calc_range (this.values, x_intervals),
-        data_frequency = calc_frequency (this.values, x_range, x_intervals),
-        y_range = calc_range (data_frequency, y_intervals);
 
-    console.log(x_range);
-    console.log(data_frequency);
-    console.log(y_range);
+    this.set('data_scale', calc_scale (x_range, intervals, false));
 
-    return this.values;
+    var data_frequency = calc_frequency (this.values, x_range, intervals);
+    this.set('data_frequency', data_frequency);
+
+    var y_range = calc_range (data_frequency, y_intervals),
+        y_scale = calc_scale (y_range, y_intervals, true);
+    this.set('y_scale_max', y_range[1]);
+    return y_scale;
 
 
-
-    function calc_range (data, intervals) { // fix later to handle neg numbers
-      var range = calc_extremes(data);
-      var scale_min = Math.floor(range[0]/intervals); // default 0
-      var size = range[1] - scale_min;
-      var step = Math.ceil(size/intervals); // default x-intervals
-      var scale_max = scale_min + (step * intervals);
-      return [scale_min, scale_max, step];
-    }
+    // helper functions
     function calc_frequency (data, x_range, x_intervals) {
       var bins = new Array(x_intervals).fill(0),
           bin_size = x_range[2];
@@ -42,6 +42,30 @@ export default Ember.Component.extend({
         bins[bin]++;
       }
       return bins;
+    }
+    function calc_scale (range, intervals, reverse) {
+      var scale = new Array(intervals + 1),
+          min = range[0],
+          max = range[1],
+          step = range[2];
+      if(reverse) {
+        for (var i = intervals; i >= 0; i--) {
+          scale[i] =  (step * (intervals - i)) + min ;
+        }
+      } else {
+        for (var i = 0; i < intervals; i++) {
+          scale[i] =  (step * (i+1)) + min ;
+        }
+      }
+      return scale;
+    }
+    function calc_range (data, intervals) {
+      var range = calc_extremes(data);
+      var scale_min = Math.floor(range[0]/intervals); // default 0
+      var size = range[1] - scale_min;
+      var step = Math.ceil(size/intervals); // default x-intervals
+      var scale_max = scale_min + (step * intervals);
+      return [scale_min, scale_max, step];
     }
     function calc_extremes (data) {
       var min = data[0],
@@ -57,76 +81,4 @@ export default Ember.Component.extend({
       return [min, max];
     }
   }.property()
-
-  // frequency: function () {
-  //   var _this = this;
-  //   var data = this.values;
-  //   var x_range = calc_range(data);
-  //   var x_step = calc_step(x_range, this.x_intervals);
-  //   var x_scale = get_x_scale(x_step, this.x_intervals);
-  //   if(x_scale) { this.set('x_scale', x_scale); }
-  //   var data_frequency = plot_frequency (data, this.x_intervals, this.y_intervals);
-  //   console.log(data_frequency);
-  //
-  //
-  //
-  //
-  //
-  //
-  //   function plot_frequency (data, x_intervals, y_intervals) {
-  //     var bins = new Array(x_intervals).fill(0);
-      // var bin_size = Math.ceil(data[1] - data[0]/x_intervals);
-      // for (var i = 0; i < data.length; i++) {
-      //   var bin = Math.ceil(data[i]/bin_size) - 1;
-      //   bins[bin]++;
-      // }
-  //
-  //     var y_range = calc_range(bins);
-  //     var y_scale = new Array(y_intervals + 1);
-  //     y_scale[y_intervals] = 0; // lowest frequency is 0
-  //     for (var i = 0; i < y_intervals.length; i ++) {
-  //       console.log('** ' + (i * y_step));
-  //       y_scale[y_intervals - i] = i * y_step;
-  //     }
-  //
-  //     return bins;
-  //   }
-  //   //set x_scale values
-  //   function get_x_scale (x_step, intervals) {
-  //     x_scale = new Array(intervals);
-  //     for (var i = 1; i <= intervals; i++) {
-  //       x_scale[i] = i * x_step;
-  //     }
-  //     return x_scale;
-  //   }
-  //   //set y_scale values
-  //   function get_y_scale (y_step, intervals, data_frequency) {
-  //     var y_scale = new Array(intervals + 1).fill(0);
-  //     for (var i = intervals; i >= 0; i--) {
-  //       y_scale[i] = y_step * i;
-  //     }
-  //     return y_scale;
-  //   }
-  //
-  //
-  //
-  //
-  //
-  //
-  //
-  //   //
-  //   function calc_range (array) {
-  //     array.sort();
-  //     return [array[0], array[array.length-1]];
-  //   }
-  //   function calc_step (range,intervals) {
-  //     var scale_min = Math.floor(range[0]/intervals);
-  //     var step = Math.ceil((range[1] - scale_min) / intervals);
-  //     return step;
-  //   }
-  //   //
-  //   //
-  //   return this.values;
-  // }.property()
-
 });
