435522b295cb84b4c24da4918bc792a169a4e4f5
aliciakw <alicia.willett@gmail.com>
2016-04-15 19:59:44 -0400

    Fix NaN error when data is null

--- app/components/freq-chart.js
+++ app/components/freq-chart.js
@@ -1,15 +1,16 @@
 import Ember from 'ember';
+import $ from "jquery";
 
 export default Ember.Component.extend({
   data_scale: [],
   data_frequency: [],
   y_intervals: 5,
   x_intervals: 6,
-  y_scale_max: 0,
+  y_scale_max: 5,
   chart_height: 170,
   chart_width: 200,
   chart_body: function () {
-    return Ember.String.htmlSafe('height:'+ (this.chart_height + 10) +'px; width:' + (this.chart_width + 30) + 'px;')
+    return Ember.String.htmlSafe('height:'+ (this.chart_height + 10) +'px; width:' + (this.chart_width + 30) + 'px;');
   }.property(),
   bar_width: function () {
     return Ember.String.htmlSafe('width:'+ ((this.chart_width) / this.x_intervals) +'px;');
@@ -18,67 +19,64 @@ export default Ember.Component.extend({
     return Ember.String.htmlSafe('height:' + ((this.chart_height) / this.y_intervals) + 'px;');
   }.property(),
   frequency_scale: function () {
-    var x_range = calc_range(this.values, this.x_intervals),
-        intervals = this.x_intervals,
-        y_intervals = this.y_intervals;
+    var data = this.values,
+        x_intervals = this.x_intervals,
+        y_intervals = this.y_intervals,
+        data_range = calc_range(data, x_intervals),
+        data_scale = calc_scale(data_range.start, data_range.step, x_intervals, false),
+        data_frequency = calc_frequency (data, data_range.start, data_range.step, x_intervals),
+        freq_range = calc_range (data_frequency, y_intervals),
+        freq_scale = calc_scale (freq_range.start, freq_range.step, y_intervals, true),
+        freq_scale_max = (y_intervals * freq_range.step) + freq_range.start ;
 
-    this.set('data_scale', calc_scale (x_range, intervals, false));
-
-    var data_frequency = calc_frequency (this.values, x_range, intervals);
     this.set('data_frequency', data_frequency);
-
-    var y_range = calc_range (data_frequency, y_intervals),
-        y_scale = calc_scale (y_range, y_intervals, true);
-    this.set('y_scale_max', y_range[1]);
-    return y_scale;
+    this.set('data_scale', data_scale);
+    this.set('y_scale_max', freq_scale_max);
+    return freq_scale;
 
 
     // helper functions
-    function calc_frequency (data, x_range, x_intervals) {
-      var bins = new Array(x_intervals).fill(0),
-          bin_size = x_range[2];
-      for (var i = 0; i < data.length; i++) {
-        var bin = Math.ceil(data[i]/bin_size) - 1;
-        bins[bin]++;
-      }
-      return bins;
-    }
-    function calc_scale (range, intervals, reverse) {
-      var scale = new Array(intervals + 1),
-          min = range[0],
-          max = range[1],
-          step = range[2];
-      if(reverse) {
-        for (var i = intervals; i >= 0; i--) {
-          scale[i] =  (step * (intervals - i)) + min ;
-        }
+    function calc_frequency (data, start, step, x_intervals) {
+      if (data.length) {
+        var bins = new Array(x_intervals).fill(0);
+        $(data).each(function(index, value){
+          var bin = Math.ceil(value/step) - (start/step) - 1;
+          bins[bin]++;
+        });
+        return bins;
       } else {
-        for (var i = 0; i < intervals; i++) {
-          scale[i] =  (step * (i+1)) + min ;
-        }
+        return [];
+      }
+    }
+    function calc_scale (start, step, intervals, reverse) {
+      var scale = new Array(intervals);
+      if(reverse) {
+        $(scale).each(function(index){
+          scale[index] = (step * (intervals - index)) + start;
+        });
+        scale.push(start);
+      } else {
+        $(scale).each(function(index){
+          var unit = (step * (index + 1)) + start;
+          scale[index] = unit;
+        });
       }
       return scale;
     }
     function calc_range (data, intervals) {
-      var range = calc_extremes(data);
-      var scale_min = Math.floor(range[0]/intervals); // default 0
-      var size = range[1] - scale_min;
-      var step = Math.ceil(size/intervals); // default x-intervals
-      var scale_max = scale_min + (step * intervals);
-      return [scale_min, scale_max, step];
-    }
-    function calc_extremes (data) {
-      var min = data[0],
-          max = data[1];
-      for (var i = 0; i < data.length; i++ ){
-        var value = data[i];
-        if ( value > max ) {
-          max = value;
-        } else if (value < min ) {
-          min = value;
+      var min = data.length ? data[0] : 0,
+          max = data.length ? data[0] : intervals;
+      $(data).each(function(index, value){
+        if (value < min) { min = value;
+        } else if (value > max){ max = value;
         }
-      }
-      return [min, max];
+      });
+      var scale_min = Math.floor(min/intervals) * intervals,
+          scale_max = Math.ceil(max/intervals) * intervals,
+          step = Math.ceil((scale_max - scale_min) / intervals);
+      return { start: scale_min,
+               step: step
+             };
     }
   }.property('values')
 });
